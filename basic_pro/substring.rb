ad9cf55ec32ac31f3f7de1af704488c382cf0790
temp <144443340+aamir-0@users.noreply.github.com>
2025-05-05 17:19:11 +0530

    this the second project program in ruby

---
+++ basic_pro/substring.rb
@@ -0,0 +1,18 @@
+def substring(str,dic)
+   res=Hash.new(0)
+   str_downed=str.downcase
+   dic.each do |substr|
+         arr=str_downed.scan(substr)#this will give array of matched words
+         cnt=arr.length
+         if cnt >0
+                      res[substr]+=cnt
+         end
+   end
+   print res
+end
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+ ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+ 
+ print("enter the string to be searched:")
+ som=gets 
+ substring(som, dictionary)
